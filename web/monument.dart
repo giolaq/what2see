700e8a94de63a501a31bd051fecef8f9f990aef9
Claudio d'Angelis <claudiodangelis@gmail.com>
2014-11-04 21:42:48 +0100

    New internal structure

--- web/monument.dart
+++ web/monument.dart
@@ -1,8 +1,23 @@
-class Monument {
-  String name;
-  String address;
-  String thmbUrl;
-  
-  Monument(this.name, this.address, this.thmbUrl);
-  
+import 'dart:html';
+import 'package:polymer/polymer.dart';
+import 'package:paper_elements/paper_dialog.dart';
+
+
+@CustomTag('x-monument')
+class XMonument extends PolymerElement {
+
+  @published String name = "";
+  @published String address = "";
+
+  XMonument.created() : super.created();
+
+  @override
+  void attached() {
+    super.attached();
+    // TODO: qua vanno i calcoli
+    shadowRoot.querySelector("#show").onClick.listen((_) {
+      print("asd");
+      (shadowRoot.querySelector("#dialog") as PaperDialog).toggle();
+    });
+  }
 }
