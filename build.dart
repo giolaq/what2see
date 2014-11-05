4a65507379807c81610170fd3bccc9871d0bae38
Claudio d'Angelis <claudiodangelis@gmail.com>
2014-11-05 18:56:07 +0100

    Sul branch claudio Changes to be committed:   (use "git reset HEAD <file>..." to unstage)
    	modified:   build.dart
    	modified:   web/monument.dart
    	modified:   web/monuments_list.dart
    	modified:   web/what2see.dart Please enter the commit message for your changes. Lines starting
    Sul branch claudio
    Changes to be committed:
      (use "git reset HEAD <file>..." to unstage)
    
    	modified:   build.dart
    	modified:   web/monument.dart
    	modified:   web/monuments_list.dart
    	modified:   web/what2see.dart with 'Sul branch claudio
    Changes to be committed:
      (use "git reset HEAD <file>..." to unstage)
    
    	modified:   build.dart
    	modified:   web/monument.dart
    	modified:   web/monuments_list.dart
    	modified:   web/what2see.dart' will be ignored, and an empty message aborts the commit.
    Sul branch claudio
    Changes to be committed:
      (use "git reset HEAD <file>..." to unstage)
    
    	modified:   build.dart
    	modified:   web/monument.dart
    	modified:   web/monuments_list.dart
    	modified:   web/what2see.dart
    Sul branch claudio
    Changes to be committed:
      (use "git reset HEAD <file>..." to unstage)
    
    	modified:   build.dart
    	modified:   web/monument.dart
    	modified:   web/monuments_list.dart
    	modified:   web/what2see.dart Sul branch claudio
    Sul branch claudio
    Changes to be committed:
      (use "git reset HEAD <file>..." to unstage)
    
    	modified:   build.dart
    	modified:   web/monument.dart
    	modified:   web/monuments_list.dart
    	modified:   web/what2see.dart Changes to be committed:
    Sul branch claudio
    Changes to be committed:
      (use "git reset HEAD <file>..." to unstage)
    
    	modified:   build.dart
    	modified:   web/monument.dart
    	modified:   web/monuments_list.dart
    	modified:   web/what2see.dart 	modified:   build.dart
    Sul branch claudio
    Changes to be committed:
      (use "git reset HEAD <file>..." to unstage)
    
    	modified:   build.dart
    	modified:   web/monument.dart
    	modified:   web/monuments_list.dart
    	modified:   web/what2see.dart 	modified:   web/monument.dart
    Sul branch claudio
    Changes to be committed:
      (use "git reset HEAD <file>..." to unstage)
    
    	modified:   build.dart
    	modified:   web/monument.dart
    	modified:   web/monuments_list.dart
    	modified:   web/what2see.dart 	modified:   web/monuments_list.dart
    Sul branch claudio
    Changes to be committed:
      (use "git reset HEAD <file>..." to unstage)
    
    	modified:   build.dart
    	modified:   web/monument.dart
    	modified:   web/monuments_list.dart
    	modified:   web/what2see.dart 	modified:   web/what2see.dart

--- build.dart
+++ build.dart
@@ -1,6 +1,5 @@
 import 'package:polymer/builder.dart';
-        
+
 main(args) {
-  build(entryPoints: ['web/what2see.html'],
-        options: parseOptions(args));
+  build(entryPoints: ['web/what2see.html'], options: parseOptions(args));
 }
