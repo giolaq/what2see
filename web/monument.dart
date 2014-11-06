155d49c34664a9f9514949543278e26c192d399d
Claudio d'Angelis <claudiodangelis@gmail.com>
2014-11-06 18:39:06 +0100

    Added more comments

--- web/monument.dart
+++ web/monument.dart
@@ -23,36 +23,48 @@ class XMonument extends PolymerElement {
   // This will hold the URL of the image retrieved by Google Search Images API
   @observable String imageSrc = "";
 
-
+  // Runs the create() method of its superclass (PolymerElement)
   XMonument.created() : super.created();
 
+  // Runs when the element is attached to DOM
   @override
   void attached() {
     super.attached();
-    // Load thumbnail
+    // Runs the getThumbnailSrc method to retrieve the image
     getThumbnailSrc(name).then((String src) {
       if (src.isNotEmpty) {
+        // Sets the field imageSrc to src. Changes are immediately reflected
+        // on the GUI
         this.imageSrc = src;
       }
     });
 
+    // Selects the #show element from the Shadow DOM.
     shadowRoot.querySelector("#show").onClick.listen((_) {
+      // Selects the #dialog element, casts it as a PaperDialog element and
+      // runs the .toggle() method.
       (shadowRoot.querySelector("#dialog") as PaperDialog).toggle();
     });
   }
 
+  // This method returns a Future. This means that this method doesn't return
+  // immediately a value, but it returns it when the value is available.
+  // In this case, the value is available when the function jsonp.fetch() returns.
   Future<String> getThumbnailSrc(String name) {
     var completer = new Completer();
     String BASE_URL = "https://ajax.googleapis.com/ajax/services/search/images";
     String query = Uri.encodeQueryComponent(name + " Roma");
 
     jsonp.fetch(uri: BASE_URL + "?v=1.0&q=$query&callback=?").then((response) {
+      // When the request returns a response, the Future can "complete"
       completer.complete(response['responseData']['results'][0]['url']);
     });
 
+    // Returns a value when available
     return completer.future;
   }
 
+  // This method redirects the user to Google Maps using the address of the monument
   void seeOnMaps() {
       if (address.trim().isNotEmpty) {
           window.location.assign("https://www.google.it/maps?q=" + Uri.encodeQueryComponent(address + " Roma Italia"));
--- web/monuments_list.dart
+++ web/monuments_list.dart
@@ -1,20 +1,32 @@
+// Imports the HTML library, needed for DOM manipulations, client-side http req.
 import 'dart:html';
 
+// Imports the polymer package
 import 'package:polymer/polymer.dart';
+// Imports the csvparser package
 import 'package:csvparser/csvparser.dart';
 
+// Defines the Monument class
 class Monument {
   String name;
   String address;
 
+  // Terse syntax for a constructor
   Monument(this.name, this.address);
 }
 
+// Defines this class to be a custom element.
+// The tag name will be `monuments-list`
 @CustomTag('monuments-list')
 class MonumentsList extends PolymerElement {
 
+  // This holds all the Monument class instances
   List<Monument> buffer = [];
+  // We use this as a cursor. Holds the position of the current element of buffer.
   int currentBufferPosition;
+  // When a Monument is ready to be displayed, we copy it from `buffer` to `monuments`.
+  // As this list is "observable", changes to this list will immediately reflect
+  // on the GUI
   @observable List<Monument> monuments = toObservable([]);
 
   MonumentsList.created() : super.created() {
@@ -26,11 +38,11 @@ class MonumentsList extends PolymerElement {
 
   }
 
+  // Parses the CSV, and loads the first 24 monuments
   void populateMonumentsList(String response) {
     CsvParser cp = new CsvParser(response, seperator: ";", quotemark: "\"", setHeaders: true);
 
     // Move three positions
-    // TODO: ugly!
     cp.moveNext();
     cp.moveNext();
     cp.moveNext();
@@ -49,6 +61,8 @@ class MonumentsList extends PolymerElement {
     currentBufferPosition = 24;
   }
 
+  // Triggered by the infinite scrolling function, this will load more elements
+  // e.g., will copy elements from `buffer` to `monuments`
   void loadMore() {
     int nextBufferPosition;
     for (var i = currentBufferPosition; i < currentBufferPosition + 8; i++) {
@@ -59,6 +73,5 @@ class MonumentsList extends PolymerElement {
     }
     currentBufferPosition = nextBufferPosition;
   }
-
 
 }
